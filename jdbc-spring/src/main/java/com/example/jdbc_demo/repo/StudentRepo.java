1e62a90c35b8d43e7fd4e1c3ad16edb66e6e8fc4
unknown <zaky.abdurrahman@phiraka.com>
2025-11-21 17:07:13 +0700

    finished postgres example

--- jdbc-spring/src/main/java/com/example/jdbc_demo/repo/StudentRepo.java
+++ jdbc-spring/src/main/java/com/example/jdbc_demo/repo/StudentRepo.java
@@ -1,9 +1,13 @@
 package com.example.jdbc_demo.repo;
 
 import com.example.jdbc_demo.model.Student;
+import com.example.jdbc_demo.rowmapper.StudentRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 //basically a DAO for a table
 @Repository
 public class StudentRepo {
@@ -18,5 +22,12 @@ public class StudentRepo {
         String sql = "INSERT INTO STUDENTS (roll_number, name, score) values (?, ?, ?)";
 
         template.update(sql, s.getRollNumber(), s.getName(), s.getScore());
+    }
+
+    public List<Student> findAll() {
+        String sql = "SELECT * FROM STUDENTS";
+        StudentRowMapper rowMapper = new StudentRowMapper();
+        List<Student> students = template.query(sql, rowMapper);
+        return students;
     }
 }
--- jdbc-spring/src/main/java/com/example/jdbc_demo/rowmapper/StudentRowMapper.java
+++ jdbc-spring/src/main/java/com/example/jdbc_demo/rowmapper/StudentRowMapper.java
@@ -6,7 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class StudentRowMapper implements RowMapper {
+public class StudentRowMapper implements RowMapper<Student> {
     @Override
     public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
         Student s = new Student();
