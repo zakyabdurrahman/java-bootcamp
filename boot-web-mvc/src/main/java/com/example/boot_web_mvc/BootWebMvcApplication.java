5b770fff9032c741f198a837fe6f232351b6e226
unknown <zaky.abdurrahman@phiraka.com>
2025-12-04 14:13:43 +0700

    added web starter

---
+++ boot-web-mvc/src/main/java/com/example/boot_web_mvc/BootWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot_web_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootWebMvcApplication.class, args);
+	}
+
+}
---
+++ boot-web-mvc/src/test/java/com/example/boot_web_mvc/BootWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot_web_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
