a3b6da16fc2c417b81828a34e918e9ded8c4c6a7
unknown <zaky.abdurrahman@phiraka.com>
2025-12-09 09:38:08 +0700

    add webform in pebbles

--- boot-web-mvc/src/main/java/com/example/boot_web_mvc/HomeController.java
+++ boot-web-mvc/src/main/java/com/example/boot_web_mvc/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.boot_web_mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
     public String home() {
+        String name = "Zaky Horts";
         return "index";
     }
 }
