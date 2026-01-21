3cc1ea2cf5f1a60de22a24992ba2da5a0cdef5b2
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2026-01-21 22:52:31 +0700

    added notes and MVC learning

--- boot-web-mvc/src/main/java/com/example/boot_web_mvc/HomeController.java
+++ boot-web-mvc/src/main/java/com/example/boot_web_mvc/HomeController.java
@@ -1,13 +1,26 @@
 package com.example.boot_web_mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String home() {
-        String name = "Zaky Horts";
+    @GetMapping("/")
+    public String home(Model model) {
+
+
         return "index";
     }
+
+    @GetMapping("add")
+    public String add(@RequestParam() Integer num1, @RequestParam() Integer num2, Model model) {
+        model.addAttribute("result", num1 + num2);
+
+        return "result";
+    }
+
+
 }
